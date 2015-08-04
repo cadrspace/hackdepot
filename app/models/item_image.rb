24c25fbd5f62fe647dbcbe92b9b3c2a862c6a9e6
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2015-08-04 21:16:32 +0300

    app/models/item_image.rb: Cleanup

--- app/models/item_image.rb
+++ app/models/item_image.rb
@@ -4,10 +4,9 @@ class ItemImage
   include Mongoid::Paperclip
 
   embedded_in :item, :inverse_of => :item_images
-  
+
   has_mongoid_attached_file :image, styles: {thumb: "100x100#"}, :default_url => "missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   validates_attachment_presence :image
   validates_attachment_size :image, :less_than => 5.megabytes
-  
 end
