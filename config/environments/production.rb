3b3353ac98311b8e411ce84f91044322770d1160
overmind <seogarry@gmail.com>
2015-11-04 18:12:00 +0300

    Remove relative path prefix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-
-  config.action_controller.relative_url_root = '/hackdepot'
 end
