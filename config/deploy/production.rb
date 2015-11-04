990b6f84aa12dc91915bd9ecde5847ecea1ca06d
overmind <seogarry@gmail.com>
2015-11-04 18:18:27 +0300

    Fix typo

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -12,7 +12,7 @@ set :nginx_server_port, 80
 set :app_user, 'over'
 
 server 'cadrbox', user: 'over', roles: %{app db web}
-after "deploy:finished", "service:service:restart"
+after "deploy:finished", "service:restart"
 
 # role-based syntax
 # ==================
