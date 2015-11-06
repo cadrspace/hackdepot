f63e67ab9faac110eb8cac485d92f06af2d748ca
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2015-11-06 19:42:42 +0300

    config: application: Set 'api_only' to 'false'
    * config/application.rb: Set 'api_only' to 'false'.

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,7 @@ module HackstoreMongo
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.i18n.default_locale = :ru
+
+    config.api_only = false
   end
 end
