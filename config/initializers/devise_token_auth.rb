58b3249ada9d5ff6100d387e405a109d4300b081
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2015-11-07 10:33:35 +0300

    config: initializers: devise_token_auth: Enable Devise
    * config/initializers/devise_token_auth.rb: Enable standard Devise
      support.

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -19,4 +19,6 @@ DeviseTokenAuth.setup do |config|
   # example, using the default '/omniauth', the github oauth2 provider will
   # redirect successful authentications to '/omniauth/github/callback'
   #config.omniauth_prefix = "/omniauth"
+
+  config.enable_standard_devise_support = true
 end
