b26bf50f92a845f4f05ec8873ca58e3c6aa5aa68
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2016-01-13 18:27:03 +0300

    base_controller.rb: Respond to JSON
    * app/controllers/api/base_controller.rb: Respond only to JSON.

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -5,5 +5,6 @@ module Api
   class ApplicationController < ActionController::Base
     include DeviseTokenAuth::Concerns::SetUserByToken
     protect_from_forgery with: :null_session
+    respond_to :json
   end
 end
